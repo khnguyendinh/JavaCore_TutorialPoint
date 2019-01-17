1e28d2ac8c9a8e17cf75c1769c51e4e9b5919c6a
khoa <khoand@ciber.vn>
2019-01-17 16:38:04 +0700

    collection và generic

---
+++ src/Collections/HashSet_Demo.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.HashSet;
+
+public class HashSet_Demo {
+    public static void main(String args[]) {
+        // create a hash set
+        HashSet hs = new HashSet();
+
+        // add elements to the hash set
+        hs.add("B");
+        hs.add("A");
+        hs.add("D");
+        hs.add("E");
+        hs.add("C");
+        hs.add("F");
+        System.out.println(hs);
+    }
+}
