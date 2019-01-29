b8db71aa2768d0a8cf2afedabfe1335c3df5cb80
khoa <khoand@ciber.vn>
2019-01-29 10:00:58 +0700

    send Email

--- src/Sending_Email/SendEmail.java
+++ src/Sending_Email/SendEmail.java
@@ -7,40 +7,71 @@ import javax.activation.*;
 
 public class SendEmail {public static void main(String [] args) {
     // Recipient's email ID needs to be mentioned.
-    String to = "abcd@gmail.com";
+    String to = "khnguyendinh@gmail.com";
 
     // Sender's email ID needs to be mentioned
-    String from = "web@gmail.com";
+    String from = "khoas401@gmail.com";
 
     // Assuming you are sending email from localhost
     String host = "localhost";
 
     // Get system properties
-    Properties properties = System.getProperties();
+    Properties prop = System.getProperties();
 
     // Setup mail server
-    properties.setProperty("mail.smtp.host", host);
+//    prop.setProperty("mail.smtp.host", host);
+//    prop.setProperty("mail.user", "khoas401@gmail.com");
+//    prop.setProperty("mail.password", "MaiMai92");
+    prop.put("mail.smtp.auth", true);
+    prop.put("mail.smtp.starttls.enable", "true");
+    prop.put("mail.smtp.host", "smtp.mailtrap.io");
+    prop.put("mail.smtp.port", "25");
+    prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
 
     // Get the default Session object.
-    Session session = Session.getDefaultInstance(properties);
+//    Session session = Session.getDefaultInstance(prop);
+    Session session = Session.getInstance(prop, new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+//            return new PasswordAuthentication("khoas401", "MaiMai92");
+            return new PasswordAuthentication("khoas401@gmail.com", "qmggzofncgwaiuqn");
+        }
+    });
 
     try {
-        // Create a default MimeMessage object.
-        MimeMessage message = new MimeMessage(session);
-
-        // Set From: header field of the header.
+//        // Create a default MimeMessage object.
+//        MimeMessage message = new MimeMessage(session);
+//
+//        // Set From: header field of the header.
+//        message.setFrom(new InternetAddress(from));
+//
+//        // Set To: header field of the header.
+//        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//        // Set Subject: header field
+//        message.setSubject("This is the Subject Line!");
+//
+//        // Now set the actual message
+//        message.setText("This is actual message");
+//
+//        // Send message
+//        Transport.send(message);
+        Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(from));
+        message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject("Mail Subject");
 
-        // Set To: header field of the header.
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        String msg = "This is my first email using JavaMailer";
 
-        // Set Subject: header field
-        message.setSubject("This is the Subject Line!");
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
 
-        // Now set the actual message
-        message.setText("This is actual message");
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
 
-        // Send message
+        message.setContent(multipart);
+
         Transport.send(message);
         System.out.println("Sent message successfully....");
     } catch (MessagingException mex) {
