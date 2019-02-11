7b2c0550dd358e490be91d9e7404ebccc3c546f9
khnguyendinh <khnguyendinh@gmail.com>
2019-02-11 16:32:34 +0700

    add streams.java

--- src/Java_8/Lambda_expression/Streams.java
+++ src/Java_8/Lambda_expression/Streams.java
@@ -1,12 +1,14 @@
 package Java_8.Lambda_expression;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Streams {
     public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1,2,13,4,15,6,17,8,19);
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
         List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
         filtered.forEach(System.out::println);
@@ -38,5 +40,22 @@ public class Streams {
         System.out.println("Merged String: " + mergedString);
 
         //https://www.tutorialspoint.com/java8/java8_streams.htm
+        squaresList = numbers.stream().map( i ->i*i).distinct().collect(Collectors.toList());
+        System.out.println("Squares List: " + squaresList);
+        System.out.println("List: " +integers);
+
+        IntSummaryStatistics stats = integers.stream().mapToInt((x) ->x).summaryStatistics();
+
+        System.out.println("Highest number in List : " + stats.getMax());
+        System.out.println("Lowest number in List : " + stats.getMin());
+        System.out.println("Sum of all numbers : " + stats.getSum());
+        System.out.println("Average of all numbers : " + stats.getAverage());
+        System.out.println("Random Numbers: ");
+
+        random.ints().limit(10).sorted().forEach(System.out::println);
+
+        //parallel processing
+        count = strings.parallelStream().filter(string -> string.isEmpty()).count();
+        System.out.println("Empty Strings: " + count);
     }
 }
